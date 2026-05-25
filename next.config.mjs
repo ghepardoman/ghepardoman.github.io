2d9d90a144139580d9d35da23fb532f634604af6
Giovanni De Rosa <82837714+ghepardoman@users.noreply.github.com>
2026-05-25 02:53:17 +0200

    Add output export option to next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',      
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
-    unoptimized: true,
+    unoptimized: true,    
   },
 }
 
